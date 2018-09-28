9ba7a1ae1ae5f643ff4d78e6e5df23ac6467a921
GitArcher <archinux01@gmail.com>
2018-09-28 15:45:40 +0000

    reorg

--- res/index.js
+++ res/index.js
@@ -1,34 +1,18 @@
 "use strict"
 let [btnMenu, btnLogo, btnCall] = [...document.querySelectorAll('header > div')];
 let imgMenu = document.querySelector('header img');
-let mains = document.querySelectorAll('main > div:not(.main)');
-let main = document.querySelector('.main');
 let menu = document.querySelector('.menu');
-let menuCall = document.getElementsByClassName('menu-call')[0];
-let menuList = document.querySelectorAll('.menu-content div');
-let content = document.getElementsByClassName('content')[0];
-let closeSlider = document.querySelector('.close');
-let dir = document.querySelector('.dir');
-let msgOfLoad = document.querySelector('.load');
-let slideShowImgs = ["View1.jpg","View3.jpg","bedroom2.1.jpg","bedroom2.3.jpg"];
-let dirsImg = [
-  ["b1.jpg","b2.jpg","k1.jpg","k2.jpg","k3.jpg","lr1.jpg","lr2.jpg"],
-  ["bedroom2.1.jpg","bedroom2.3.jpg","bedroom2.4.jpg"],
-  ["gostinaya1.jpg","gostinaya2.jpg","gostinaya3.jpg","gostinaya4.jpg"],
-  ["View1.jpg","View3.jpg"]
-];
-let id, img = [];
-let lastClickedEl;
 
-//cacheImages("./res/photos/slideShow/", slideShowImgs);
+(function preLoad() {
+  initBtnsMenu();
+  initlazyImgs();
+})();
+
 window.onload = () => {
   hideMsgOfLoad();
-  lazyImgs();
-  id = slideShowStart(slideShowImgs);
-  initBtnsMenu();
+  slideShowStart();
   initBtnDirs();
   SliderInit();
-//  initSwipeMenu();
 };
 
 function cacheImages(path, arr) {
@@ -38,7 +22,7 @@ function cacheImages(path, arr) {
   };
 };
 
-function lazyImgs() {
+function initlazyImgs() {
   let imgs = document.querySelectorAll('img[data-src]');
   for (let img of imgs) {
     img.src = img.getAttribute('data-src');
@@ -48,188 +32,45 @@ function lazyImgs() {
   };
 };
 
-function initBtnDirs() {
-  let dirsBtn = document.querySelectorAll('.dirs div');
-  let slider = document.querySelector('.slider');
-
-  for (let i = 0; i < dirsBtn.length; i++) {
-    dirsBtn[i].onclick = () => {
-      createImgsInDiv(`./res/photos/dir${i+1}/`, dirsImg[i], dir);
-      slider.classList.remove('hide');
-    };
-  };
-};
-
-function createImgsInDiv(path, arr, parent) {
-  parent.innerHTML = null;
-  parent.style.width = `${100*arr.length}%`;
-  let img;
-
-  for (let name of arr) {
-    img = document.createElement('img');
-    img.src = `${path+name}`;
-    img.style.width = `${100/arr.length}%`;
-    img.classList.add('animate');
-    parent.appendChild(img);
-  };
-};
-
-function SliderInit() {
-  let slider = document.querySelector('.slider');
-  dir.translateX = 0;
-  dir.step = 1;
-  dir.steps = () => dir.childElementCount;
-  dir.rate = () => 100/dir.childElementCount;
-
-  slider.next = () => {
-    if ( dir.step !== dir.steps() ) {
-      dir.translateX -= dir.rate();
-      dir.style.transform = `translateX(${dir.translateX}%)`;
-      dir.step++
-    };
-  };
-
-  slider.back = () => {
-    if (dir.step !== 1) {
-      dir.translateX += dir.rate();
-      dir.style.transform = `translateX(${dir.translateX}%)`;
-      dir.step--;
-    };
-  };
-
-  document.querySelector('#btnBack').onclick = slider.back;
-  document.querySelector('#btnNext').onclick = slider.next;
-
-  closeSlider.onclick = (e) => {
-    dir.translateX = 0;
-    dir.step = 1;
-    slider.classList.add('hide');
-    dir.style.transform = 'translateX(0)';
-  };
-
-  initSwapSlide(slider)
-};
-
-function initSwapSlide(slider) {
-  let x, startX, requestID;
-
-  dir.ontouchstart = (e) => {
-    e.preventDefault();
-    x = 0;
-    startX = e.touches[0].clientX;
-    dir.classList.remove('animate');
-
-    (function step () {
-// FIXME:
-      if ( (x > 0 && dir.step == 1) || (x < 0 && dir.step == dir.steps) ) {
-        requestID = requestAnimationFrame(step);
-        return;
-      }
-
-      dir.style.transform = `translateX( calc(${x}px + ${dir.translateX}%) )`;
-      requestID = requestAnimationFrame(step);
-    })()
-  };
-
-  dir.ontouchmove = (e) => {
-    e.preventDefault();
-    x = Math.round(e.touches[0].clientX - startX);
-  };
-
-  dir.ontouchend = () => {
-    window.cancelAnimationFrame(requestID);
-    dir.classList.add('animate');
-
-    if (x > 0) {
-      slider.back();
-    };
-    if (x < 0) {
-      slider.next();
-    };
-  };
-};
-
 function initBtnsMenu() {
-  lastClickedEl = main;
-// FIXME:
+  let main = document.querySelector('.main');
+  let lastClickedEl = main;
+
+  let mains = document.querySelectorAll('main > div:not(.main)');
+  let menuList = document.querySelectorAll('.menu-content div');
+
   for (let i = 0; i < menuList.length; i++) {
     menuList[i].onclick = () => {
-      lastClickedEl.style.display = "none";
+      lastClickedEl.classList.remove('visible');
       lastClickedEl = mains[i];
-      let btnDisplay = getComputedStyle(mains[i]).display;
-      mains[i].style.display = btnDisplay == "none" ? "flex" : "none";
-      hideMenuArea()
-      clearInterval(id);
-      menuList[i] == menuList[0] ? id = slideShowStart(slider) : null;
+      mains[i].classList.toggle('visible');
+      hideMenu();
     };
   };
-};
 
-function initSwipeMenu() {
-  let x, startX, requestID;
-
-  window.addEventListener('touchstart', (e) => {
-    x = 0;
-    startX = e.touches[0].clientX;
-
-    if (startX/innerWidth < 0.06 && menu.classList.contains('left-hide')) {
-      menu.translateX = -innerWidth/3;
-      menu.classList.remove('animate');
-
-      (function step() {
-        if (x < innerWidth/3) {
-          menu.style.transform = `translateX(${x + menu.translateX}px)`;
-          requestID = requestAnimationFrame(step);
-        };
-      })();
-    };
-  });
-  menu.addEventListener('touchstart', (e) => {
-    if (!menu.classList.contains('left-hide')) {
-      menu.classList.remove('animate');
-
-    };
-  });
-
-  window.addEventListener('touchmove', (e) => {
-//    e.preventDefault();
-    x = Math.round(e.touches[0].clientX - startX);
-  });
-
-  window.addEventListener('touchend', () => {
-    cancelAnimationFrame(requestID);
-    menu.style.removeProperty('transform');
-    menu.classList.add('animate');
-    if (x > startX) {
-      menu.classList.remove('left-hide');
-    };
-  });
-};
-
-function slideShowStart (arr) {
-  let imgs = document.querySelectorAll('.main img')
-  let i = 1;
-  let lastI = imgs[0];
-
-  return setInterval( () => {
-    lastI.classList.remove('show');
-    imgs[i].classList.add('show');
-    lastI = imgs[i];
-    ++i == 4 ? i = 0 : null;
-  }, 6000);
+  btnLogo.onclick = () => {
+    lastClickedEl.classList.remove('visible');
+    main.classList.add('visible');
+    lastClickedEl = main;
+    hideMenu();
+  };
 };
 
 function hideMsgOfLoad() {
+  let msgOfLoad = document.querySelector('.load');
   msgOfLoad.style.opacity = 0;
-  setTimeout(() => {
-    msgOfLoad.style.display = "none";
-  }, 1100)
-}
 
-function hideMenuArea() {
+  setTimeout(() => {
+//    msgOfLoad.style.display = "none";
+    document.body.removeChild(msgOfLoad);
+  }, 1100);
+};
+
+function hideMenu() {
   btnMenu.classList.remove('btn-active');
   menu.classList.add('left-hide');
   imgMenu.src = './res/icons/menu.png';
+  document.getElementById('close').classList.add('none');
 };
 
 btnMenu.onclick = () => {
@@ -240,27 +81,12 @@ btnMenu.onclick = () => {
   } else {
     imgMenu.src = './res/icons/menu.png';
   };
+  document.getElementById('close').classList.toggle('none');
 }
 
-btnLogo.onclick = () => {
-  lastClickedEl.style.display = "none";
-  main.style.display = "flex";
-  lastClickedEl = main;
-  clearInterval(id);
-  id = slideShowStart(slider);
-};
-
 btnCall.onclick = () => {
-  btnCall.classList.toggle('btn-call-active');
+  let menuCall = document.querySelector('.menu-call');
   menuCall.classList.toggle('top-hide')
-};
-
-window.onclick = (e) => {
-  let arr = Array.from(e.path)
-  arr = arr.filter( el => el == menu )
-
-  let isMenuArea = e.path.some( elem => {
-    return elem == btnMenu || elem == menu;
-  });
-  if (!isMenuArea) hideMenuArea();
+  btnCall.classList.toggle('btn-call-active');
+  hideMenu();
 };
